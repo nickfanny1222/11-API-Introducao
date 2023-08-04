996360e87eec93c298e3ee11eeeaec0ce0885e4c
nickfanny1222 <132698504+nickfanny1222@users.noreply.github.com>
2023-08-04 16:39:08 +0000

    mudando estrutura

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //index.js
 import dotenv from "dotenv";
-dotenv.config();
 import { selectUsuarios } from "./bd.js";
 import express from "express";      // Requisição do pacote do express
+
+dotenv.config();
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -13,9 +15,6 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
   try {
@@ -24,4 +23,8 @@ app.get("/usuarios", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
