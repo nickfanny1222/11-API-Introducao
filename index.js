ad438f0f3669613f4d15c88d77f8eb6a7f66d1e0
nickfanny1222 <132698504+nickfanny1222@users.noreply.github.com>
2023-08-04 14:00:56 +0000

    adicionando codigo

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { selectUsuarios } from "./bd.js";
-app.get("/usuarios", async (req, res) => {
-  console.log("Rota GET/usuarios solicitada");
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
@@ -24,4 +15,13 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
